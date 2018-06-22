37f7f4468db3fbec3efb9069c572b3bab11f0c25
Dendy Satrya Wibowo <39975972+dendysatrya@users.noreply.github.com>
2018-06-22 23:59:03 +0700

    Adding Empty Class
    Menambah class doang

--- application/controllers/barang.php
+++ application/controllers/barang.php
@@ -1,1 +1,18 @@
-<?php ?>
+<?php 
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Barang extends CI_Controller {
+
+	public function index()
+	{
+		
+	}
+
+}
+
+/* End of file barang.php */
+/* Location: ./application/controllers/barang.php */
+
+
+?>
